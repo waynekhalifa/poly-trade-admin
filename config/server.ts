38e124cc66e92e649ba294fa1680ed524e8579b7
Wani Joseph <waynekhalifa.wk@gmail.com>
2024-06-28 21:16:45 +0200

    updates server posts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  url: env("PUBLIC_URL", "http://localhost:1339"),
+  url: env("PUBLIC_URL", "http://localhost:1337"),
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1339),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
